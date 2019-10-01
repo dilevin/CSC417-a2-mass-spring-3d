b1c6447986a58f45b41196aef46b3ab5f231e25d
HTDerekLiu <hsuehtil@cs.toronto.edu>
2019-10-01 10:59:56 -0400

    update dV_gravity_particle_dq inputs

--- src/dV_gravity_particle_dq.cpp
+++ src/dV_gravity_particle_dq.cpp
@@ -1,6 +1,6 @@
 #include <dV_gravity_particle_dq.h>
 
-void dV_gravity_particle_dq(Eigen::Ref<Eigen::Vector3d> f, Eigen::Ref<const Eigen::Vector3d> q,  double mass, Eigen::Ref<const Eigen::Vector3d> g) {
+void dV_gravity_particle_dq(Eigen::Ref<Eigen::Vector3d> f,  double mass, Eigen::Ref<const Eigen::Vector3d> g) {
 
 
 }
