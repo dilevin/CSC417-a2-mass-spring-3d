24bc240df9f45d527aec8ed610d7c7008b50362b
HTDerekLiu <hsuehtil@cs.toronto.edu>
2019-10-06 11:23:52 -0400

    correct typos

--- include/V_gravity_particle.h
+++ include/V_gravity_particle.h
@@ -3,7 +3,7 @@
 //Input:
 //  q - generalized coordinate of a particle
 //  mass - the mass of particles in the mass spring system
-//  q - the gravity acceleration vector
+//  g - the gravity acceleration vector
 //Output:
 //  V - the potential energy due to gravity acting on this particle
 void V_gravity_particle(double &V, Eigen::Ref<const Eigen::Vector3d> q,  double mass, Eigen::Ref<const Eigen::Vector3d> g);
