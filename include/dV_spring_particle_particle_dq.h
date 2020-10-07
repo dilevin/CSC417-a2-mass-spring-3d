2b6645226f86f3cee4bf6013150a848a49634c21
Abhishek Madan <abhi.madan01@gmail.com>
2020-10-07 00:03:57 -0400

    Fix comment in dV_spring

--- include/dV_spring_particle_particle_dq.h
+++ include/dV_spring_particle_particle_dq.h
@@ -7,5 +7,5 @@
 //  l0 - the undeformed length of the spring
 //  stiffness - the stiffness constant for this spring
 //Output:
-//  f - the 6x1 per spring generalized force vector
+//  f - the 6x1 per spring generalized energy gradient vector
 void dV_spring_particle_particle_dq(Eigen::Ref<Eigen::Vector6d> f, Eigen::Ref<const Eigen::Vector3d> q0,  Eigen::Ref<const Eigen::Vector3d> q1, double l0, double stiffness);
